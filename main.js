4cedcdfca604dc659058e401d2380214dd6fd159
habitussolucoes-byte <habitussolucoes@gmail.com>
2025-12-29 15:52:48 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => (
+  <div style={{ padding: 20 }}>
+    <h1>Gerenciador TV Online</h1>
+    <p>App carregado com sucesso 🎉</p>
+  </div>
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
